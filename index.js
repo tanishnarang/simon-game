910f1800695ac8ab72d71a96a7444264a34ccd5c
Tanish Narang <narangtanish2@gmail.com>
2024-01-26 16:56:03 +0530

    sound added to button and saved user clicked pattern

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
+var userClickedPattern = [];
 
 function nextSequence() {
 
@@ -9,11 +10,23 @@ function nextSequence() {
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  //1. Use jQuery to select the button with the same id as the randomChosenColour
-  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+  
+  playsound(randomChosenColour)
+}
 
-  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+$(".btn").click(function(){
+  var userChoosenColour = $(this).attr("id");
+  userClickedPattern.push(userChoosenColour);
+
+  console.log(userClickedPattern);
+
+  playsound(userchoosencolour);
+})
+
+function playsound(name){
+
+
+  var audio = new Audio("sounds/" + name +".mp3");
   audio.play();
 }
